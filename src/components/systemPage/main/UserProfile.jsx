e166adca01ecf5814071afc9e39ac2d51903c7bd
dddak0310 <06160310vicky@gmail.com>
2025-04-09 22:51:21 +0800

    修改個人資料風格

--- src/components/systemPage/main/UserProfile.jsx
+++ src/components/systemPage/main/UserProfile.jsx
@@ -84,6 +84,7 @@ function UserProfile({ user, onUpdateUser }) {
 
   return (
     <div className="relative h-screen bg-white">
+      <div className="absolute inset-0 z-0 pointer-events-none before:content-[''] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_center,_rgba(59,130,246,0.2),_transparent_70%)]" />
       {/* 藍色波浪背景 - 延伸到頂部 */}
       <div className="absolute top-0 left-0 right-0 h-[200px] overflow-hidden">
         <svg
@@ -104,7 +105,7 @@ function UserProfile({ user, onUpdateUser }) {
         <svg className="w-full" style={{ marginBottom: "-1px" }} viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path
             d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
-            className="fill-blue-100 opacity-40"
+            className="fill-blue-300 opacity-40"
           ></path>
         </svg>
       </div>
