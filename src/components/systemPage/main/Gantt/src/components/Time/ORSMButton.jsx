062c7ab9487812749bf386a675f0241aa56dda7d
HaDog <yongjun0423@gmail.com>
2025-04-24 22:31:27 +0800

    新增：排班時會跳出Loading頁面

--- src/components/systemPage/main/Gantt/src/components/Time/ORSMButton.jsx
+++ src/components/systemPage/main/Gantt/src/components/Time/ORSMButton.jsx
@@ -4,10 +4,12 @@ import { BASE_URL } from "../../../../../../../config";
 
 const ORSMButton = () => {
     const [isLoading, setIsLoading] = useState(false);
+    const [showResultButton, setShowResultButton] = useState(false);
 
     const handleRunAlgorithm = async (event) => {
         event.preventDefault();  // 防止表單提交時刷新頁面
         setIsLoading(true);
+        setShowResultButton(false)
 
         try {
             const response = await axios.get(`${BASE_URL}/api/system/algorithm/run`);
@@ -16,8 +18,8 @@ const ORSMButton = () => {
                 alert("⚠️ 錯誤：後端可能回傳了錯誤頁，請檢查是否 .bat 檔執行失敗");
                 console.warn("返回 HTML：", result);
             } else {
-                alert(result);
-                setIsLoading(false)  // 重置加載狀態
+                setIsLoading(false);
+                setShowResultButton(true);
             }
         } catch (error) {
             console.error("執行錯誤：", error);
@@ -25,30 +27,58 @@ const ORSMButton = () => {
         }
     };
 
+    const handleCloseResultButton = () => {
+        setShowResultButton(false);
+        window.location.reload(); // 重新載入頁面
+    }
 
     return (
-        <button
-            type="button"
-            className="gantt-buttons flex items-center bg-purple-500 hover:bg-purple-700 text-white font-medium py-2 px-4 rounded-md shadow-sm transition-colors duration-300"
-            onClick={(e) => handleRunAlgorithm(e)}
-            disabled={isLoading}
-        >
-            <svg
-                className="h-6 w-6 mr-1"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+        <>
+            <button
+                type="button"
+                className="gantt-buttons flex items-center bg-purple-500 hover:bg-purple-700 text-white font-medium py-2 px-4 rounded-md shadow-sm transition-colors duration-300"
+                onClick={(e) => handleRunAlgorithm(e)}
+                disabled={isLoading}
             >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 13l4 4L19 7"
-                />
-            </svg>
-            {isLoading ? '執行中...' : '開始排班'}
-        </button>
+                <svg
+                    className="h-6 w-6 mr-1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M5 13l4 4L19 7"
+                    />
+                </svg>
+                {isLoading ? '執行中...' : '開始排班'}
+            </button>
+            {isLoading && (
+                <div className="modal-overlay">
+                    <div className="spin-wrapper">
+                        <div className="spinner"></div>
+                        <div className="loading-text">排班中...</div>
+                    </div>
+                </div>)}
+
+            {showResultButton && !isLoading && (
+                <div className="modal-overlay">
+                    <div className="spin-wrapper">
+                        <div className="checkmark">
+                            <svg viewBox="0 0 52 52">
+                                <path d="M14 27 L22 35 L38 19" />
+                            </svg>
+                        </div>
+                        <button className="gantt-buttons flex items-center bg-purple-500 hover:bg-purple-700 text-white font-medium py-2 px-4 rounded-md shadow-sm transition-colors duration-300" 
+                        onClick={handleCloseResultButton}>排班完成！</button>
+                    </div>
+                </div>
+            )}
+        </>
+
     );
 };
 
